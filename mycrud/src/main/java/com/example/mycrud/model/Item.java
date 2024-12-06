34389e3e949f077f2ca65304c86daa941a4eba6a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-06 19:15:11 +0530

    Update

---
+++ mycrud/src/main/java/com/example/mycrud/model/Item.java
@@ -0,0 +1,11 @@
+package com.example.mycrud.model;
+
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+public class Item {
+    
+}
