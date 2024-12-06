586301d4d2feb8aec5b560b2880adf7439657e85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-06 19:21:34 +0530

    Update Item.java

--- mycrud/src/main/java/com/example/mycrud/model/Item.java
+++ mycrud/src/main/java/com/example/mycrud/model/Item.java
@@ -1,5 +1,7 @@
 package com.example.mycrud.model;
 
+// help mapping Java objects to relational database tables.
+
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,5 +9,27 @@ import jakarta.persistence.Id;
 
 
 public class Item {
-    
+    // primary key
+    @Id
+    // headle the Autoincrement
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String description;
+
+    // getters and setters 
+    // reseve value and update this new value
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    // if there are many feilds can add after this
+
 }
